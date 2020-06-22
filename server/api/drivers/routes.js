bac9f374f656b415f8eb32098970f117cf0c57eb
Pratik <pratik1070061@gmail.com>
2020-06-22 17:21:51 -0400

    calling database-service

--- server/api/drivers/routes.js
+++ server/api/drivers/routes.js
@@ -1,53 +1,78 @@
-const express = require('express')
-var rn = require('random-number');
-const router = express.Router({mergeParams: true})
-const dataStore = require('../../data/dataStore')
+const express = require("express");
+var rn = require("random-number");
+const router = express.Router({ mergeParams: true });
+const dataStore = require("../../data/dataStore");
+const axios = require("axios");
+const { Agent } = require("https");
 
 var gen = rn.generator({
-  min:  100000000
-, max:  999999999
-, integer: true
-})
+  min: 100000000,
+  max: 999999999,
+  integer: true,
+});
 
-router.route('/driverInfo/:id/:quoteId')
+const client = axios.create({
+  httpsAgent: new Agent({
+    rejectUnauthorized: false,
+  }),
+});
+
+router
+  .route("/driverInfo/:id/:quoteId")
   .get(async (req, res, next) => {
-    res.send(JSON.stringify(await getDriverInfo(req.params.id, reques.params.quoteId)))
+    res.send(
+      JSON.stringify(await getDriverInfo(req.params.id, reques.params.quoteId))
+    );
   })
   .post(async (req, res, next) => {
-    res.send(JSON.stringify({result : await saveDriverInfo(req.body, req.params.quoteId)}))
-  })
+    res.send(
+      JSON.stringify({
+        result: await saveDriverInfo(req.body, req.params.quoteId),
+      })
+    );
+  });
 
 let getDriverInfo = async (id, quoteId) => {
-  console.log('Returning Driver #', id)
-  let driver = await dataStore.findDriver(quoteId)
-  return driver
-}
+  console.log("Returning Driver #", id);
+  let driver = await dataStore.findDriver(quoteId);
+  return driver;
+};
 
 let saveDriverInfo = async (data, quoteId) => {
-  let driver = '';
-  if(data.id !== ''){
-    driver = await dataStore.findDriver(quoteId);
-  }else{
-    driver = {};
-    driver.quoteId = quoteId;
-  }
-  
-  driver.name = data.name
-  driver.gender = data.gender
-  driver.maritalStatus = data.maritalStatus
-  driver.employmentStatus = data.employmentStatus
-  driver.currentIns = data.currentIns
-  driver.licensedAge = data.licensedAge
-  driver.education = data.education
-  driver.licenseNum = data.licenseNum
-  
-  if(data.id === '') {
-    driver.id = gen().toString()
+  let driver = "";
+  // if(!data.id){
+  //   driver = await dataStore.findDriver(quoteId);
+  // }else{
+  driver = {};
+  driver.quoteId = quoteId;
+  // }
+
+  driver.name = data.name;
+  driver.gender = data.gender;
+  driver.maritalStatus = data.maritalStatus;
+  driver.employmentStatus = data.employmentStatus;
+  driver.currentIns = data.currentIns;
+  driver.licensedAge = data.licensedAge;
+  driver.education = data.education;
+  driver.licenseNum = data.licenseNum;
+
+  if (!data.id) {
+    driver.id = gen().toString();
   }
 
-  dataStore.addDriver(driver)
+  await client.post(
+    `${process.env.DB_SERVICE_URL}/${process.env.COLLECTION_NAME}`,
+    driver,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  // dataStore.addDriver(driver);
 
   return driver.id;
-}
+};
 
 module.exports = router;
